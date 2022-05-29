2343c04ee2e08e7317d34e5ca58daf51b66b0b08
ivan954 <78029556+ivan954@users.noreply.github.com>
2022-05-29 03:44:59 +0300

    add cors to the server

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
 import { errorHandler, notFound } from './midleware/errorMiddleware.js'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
